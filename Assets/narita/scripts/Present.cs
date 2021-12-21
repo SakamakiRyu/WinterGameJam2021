b61becb3b24ee5a939dec01324947bc4665c6064
naritayuuto <yuuto122narita@i.softbank.jp>
2021-12-21 18:18:01 +0900

    特になし

---
+++ Assets/narita/scripts/Present.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Present : MonoBehaviour
+{
+    public AudioClip Succeed;
+    public AudioClip Failure;
+    public AudioClip BadBoy;
+    private void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector3 force = new Vector3(0.3f, 0, 0);
+        rb.AddForce(force, ForceMode2D.Force);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "House")//家に当たったら音を鳴らして自分を消す
+        {
+            Debug.Log("成功！");
+            AudioSource.PlayClipAtPoint(Succeed, transform.position);
+            Destroy(this.gameObject);
+        }
+        //else if(collision.gameObject.tag == "悪人の家の名前")
+        //{
+        //    Debag.Log("悪い子だった…");
+        //    AudioSource.PlayClipAtPoint(BadBoy, transform.position);
+        //    Destroy(this.gameObject);
+        //}
+        else//家以外に当たったら音を鳴らして自分を消す
+        {
+            Debug.Log("失敗！");
+            AudioSource.PlayClipAtPoint(Failure, transform.position);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/narita/scripts/Stage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    [SerializeField] Vector3 GeneratePosition;//移動させる座標
+    [SerializeField] GameObject[] Object;
+    public static float speed = -0.01f;
+    void Update()
+    {
+        transform.Translate(speed, 0, 0);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DeadZone"))
+        {
+            int Rand = Random.Range(0, Object.Length);//0～gameobjectの配列ー１をランダムで渡す
+            GameObject Go = Instantiate(Object[Rand]);
+            Go.transform.position = GeneratePosition;
+            Debug.Log("生成成功");
+        }
+        Destroy(this.gameObject);
+    }
+}
