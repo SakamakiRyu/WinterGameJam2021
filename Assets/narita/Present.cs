f7386128cd46ac03c78ca8671275278d7dfec340
naritayuuto <yuuto122narita@i.softbank.jp>
2021-12-21 12:29:04 +0900

    プレゼントと家のscriptを追加しました。

---
+++ Assets/narita/Present.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Present : MonoBehaviour
+{
+    public AudioClip Succeed;
+    public AudioClip Failure;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "家の名前")
+        {
+            AudioSource.PlayClipAtPoint(Succeed, transform.position);
+            Destroy(this.gameObject);
+        }
+        else if(collision.gameObject.tag == "地面の名前")
+        {
+            AudioSource.PlayClipAtPoint(Failure, transform.position);
+            Destroy(this.gameObject);
+        }
+    }
+}
