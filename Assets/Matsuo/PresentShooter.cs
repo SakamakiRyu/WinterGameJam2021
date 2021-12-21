7a0984ee3dfec3e7d2792dcfb6207d7b971532ff
Roku <mr558869193@gmail.com>
2021-12-21 12:33:54 +0900

    追加：PresentShooter パーティクル　雪

---
+++ Assets/Matsuo/PresentShooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentShooter : MonoBehaviour
+{
+    [SerializeField] GameObject present = default;
+    [SerializeField] Transform _muzzle = default;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var go = Instantiate(present);
+            go.transform.position = _muzzle.position;
+
+        }
+    }
+}
