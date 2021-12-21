54380a7cdf200f2a54dff43d672ecd7f7e6c195b
Roku <mr558869193@gmail.com>
2021-12-21 12:17:23 +0900

    追加：プレゼントシューター パーティクル　雪

---
+++ Assets/Matsuo/PresentShooter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentShooter : MonoBehaviour
+{
+    [SerializeField] GameObject present = default;
+    [SerializeField] Transform _muzzle = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var go = Instantiate(present);
+            go.transform.position = _muzzle.position;
+
+        }
+    }
+}
