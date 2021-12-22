cfcbf655fffe3615933ed8673b06d2978d5cb13e
naritayuuto <yuuto122narita@i.softbank.jp>
2021-12-22 13:55:32 +0900

    EffectManagerを追加しました。※test用のメソッドあります。

---
+++ Assets/narita/scripts/EffectManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : Singleton<EffectManager>
+{
+    [SerializeField] GameObject[] Effects = default;
+
+    public enum Effect
+    {
+        None = -1,
+        Cracker,
+        Twinkle
+    }
+
+    public void PlayEffect(Effect ef, Vector3 pos, float invoke = 0f)
+    {
+        GameObject Particle = Effects[(int)ef];
+        GameObject obj = Instantiate(Particle);
+        obj.transform.position = pos;
+        if (invoke > 0f) Destroy(obj, invoke);
+    }
+
+    /// <summary>
+    /// テスト用メソッドです
+    /// </summary>
+    public void Test()
+    {
+        PlayEffect(Effect.Cracker, Vector3.zero);
+    }
+}
